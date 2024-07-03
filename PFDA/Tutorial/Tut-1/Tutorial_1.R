0bfe3a1b7e66291c9f9e78f15cf333e8b2b9ab1a
KhooLayHan <dummygithuber01@gmail.com>
2024-07-03 13:09:43 +0800

    Tutorial-1

---
+++ PFDA/Tutorial/Tut-1/Tutorial_1.R
@@ -0,0 +1,88 @@
+# Variable
+
+x_y = 9
+x_y
+
+y.z = 10
+y.z
+
+y <- 6
+y
+
+greet <- 'Hello World!'
+greet
+
+assign('greet', 'Welcome!')
+greet
+
+assign(x, 10) # wrong
+x
+
+assign('x', 100)
+x
+
+rm(x)
+rm(greet)
+rm(y)
+
+var1 = var2 = var3 = "Orange" 
+# var1 <- var2 <- var3 <- "Orange"
+
+paste('x', 'x')
+'x' + 'x'
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+str # print the value of str
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+cat(str)
+
+
+
+###################
+
+5 + 17
+10 - 7
+4 * 3 * 5
+18 / 9
+9 %% 4
+(5 * 8) + (12 - 7)
+
+###################
+
+weeks <- 4
+hoursPerWeek = 40
+assign('hourlyRate', 50)
+
+x <- 100
+rm(x)
+
+###################
+
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE
+class(x)
